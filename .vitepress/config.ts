d348a59824d6aed428295e3f074284d1cc3019d8
FLC <paulespling@gmail.com>
2026-03-07 16:12:26 +0800

    feat: add index page and tweak config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,29 +2,31 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  srcDir: "docs",
-  
-  title: "FLC's Blog",
-  description: "A Blog Site",
+  srcDir: 'docs',
+
+  title: 'FLC 的工程師生存筆記',
+  description: '對這個世界發出吶喊，即使所有人都無動於衷。',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
     sidebar: [
       {
-        text: 'Examples',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: ' 首頁', link: '/' },
+          {
+            text: 'Command Line 101',
+            collapsed: true,
+            items: [
+              { text: 'Navigation', link: '/cmd-101/navigation' },
+              {
+                text: 'Exploring the System',
+                link: '/cmd-101/exploring-the-system',
+              },
+            ],
+          },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
 })
